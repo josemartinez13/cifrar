45229854f122aa0f5a21fc82f02220219a115f66
josemartinez13 <142464958+josemartinez13@users.noreply.github.com>
2023-08-16 23:10:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function cifrar() {
+  const textoEntrada = document.getElementById("textoEntrada").value;
+  const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+  const textoSalida = document.getElementById("textoSalida");
+  const textoCifrado = cifrarTexto(textoEntrada, desplazamiento);
+  textoSalida.value = textoCifrado;
+}
+
+function descifrar() {
+  const textoCifrado = document.getElementById("textoEntrada").value; // Cambio aquí
+  const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+  const textoSalida = document.getElementById("textoSalida");
+  const textoDescifrado = descifrarTexto(textoCifrado, desplazamiento); // Cambio aquí
+  textoSalida.value = textoDescifrado;
+}
+
+function cifrarTexto(texto, desplazamiento) {
+  return texto
+    .split("")
+    .map(caracter => {
+      if (caracter.match(/[a-zA-Z]/)) {
+        const codigo = caracter.charCodeAt(0);
+        const esMayuscula = caracter === caracter.toUpperCase();
+        const codigoDesplazado = ((codigo - (esMayuscula ? 65 : 97) + desplazamiento) % 26) + (esMayuscula ? 65 : 97);
+        return String.fromCharCode(codigoDesplazado);
+      } else {
+        return caracter; // Mantener caracteres no alfabéticos sin cambio
+      }
+    })
+    .join("");
+}
+
+function descifrarTexto(texto, desplazamiento) {
+  return cifrarTexto(texto, 26 - desplazamiento);
+}
